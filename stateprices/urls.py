27055832845de4f55a696f7a154f8c98706680ac
Manuel Alvarez <escriva@gmail.com>
2015-01-29 13:01:46 +0100

    First hello world/home view

--- stateprices/urls.py
+++ stateprices/urls.py
@@ -2,16 +2,19 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
+from stateprices.views import HomeView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'stateprices.views.home', name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     # url(r'^stateprices/', include('stateprices.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^sp-admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^sp-admin/', include(admin.site.urls)),
 )
